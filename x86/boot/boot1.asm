1c1edb73bc0d9634ab745e61927b2f2c2948aa48
lupeeumeeu <lucasperovani@gmail.com>
2017-03-14 00:31:28 -0300

    Update boot1.asm

--- x86/boot/boot1.asm
+++ x86/boot/boot1.asm
@@ -1,1 +1,21 @@
+;################################################
+;#                                              #
+;#                                              #
+;#       Project: SIZES @ 2017 - boot1.asm      #
+;#                                              #
+;#               Made by: Lucas P.              #
+;#                                              #
+;#                                              #
+;################################################
 
+
+org 0x7C00      ;BIOS put us here
+bits 16         ;We start in 16 bits mode
+
+
+;       FAT 32 FS just for now
+
+
+Begin: jmp Main                           ;Jump to executable area
+OEMName               db "SIZES P>"       ;OS Name, can be whatever
+BytespSector          
